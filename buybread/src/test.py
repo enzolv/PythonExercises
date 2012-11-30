17df7ae654fda3cb0d742931e937162aa4d2ab5e
ROLIS <ROLIS@10.140.81.250>
2012-11-30 11:21:39 +0100

    test

---
+++ buybread/src/test.py
@@ -0,0 +1,21 @@
+food=[]
+store = ["bread","butter","cheese"]
+
+def bbread ():
+    if "bread" in store:
+        return "bread"
+    return None
+    pass
+
+def bsausage ():
+    if "sausage" in store:
+        return "sausage"
+    return None
+    pass
+
+def bbutter ():
+    if "butter" in store:
+        return "butter"
+    return None
+    pass
+
